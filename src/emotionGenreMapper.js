d895da57773e73504157b3cf18b9bb697d3a4c35
Cody Jenkins <iknowcss@gmail.com>
2016-09-15 18:46:29 +1000

    Clean up emotion genre mapping tests

--- src/emotionGenreMapper.js
+++ src/emotionGenreMapper.js
@@ -1,48 +1,19 @@
-var forEach = require('lodash/forEach');
-var intersection = require('lodash/intersection');
+const forEach = require('lodash/forEach');
+const intersection = require('lodash/intersection');
 
-var genre_emotions = require('./data/genre_emotions.json')
+var genreEmotionMap = require('./data/genre_emotions.json');
 
 function genresforEmotions(emotions) {
+  var matchingGenres = [];
 
-  var matching_genres = [];
-
-  // console.log("looking for genres for these emotions:"+emotions);
-
-  forEach(genre_emotions, function (genreEmotions, genre) {
+  forEach(genreEmotionMap, function (genreEmotions, genre) {
     if (intersection(emotions, genreEmotions).length === emotions.length) {
-      matching_genres.push(genre);
+      matchingGenres.push(genre);
     }
   });
-
-  // for (var genreKey in genre_emotions) {
-  //
-  //   //console.log("checking genre:" + genreKey + ", value:" + genre_emotions[genreKey]);
-  //
-  //   var matches = false;
-  //
-  //   emotions.forEach(function (emotion) {
-  //     matches = false;
-  //     if (genre_emotions[genreKey].indexOf(emotion) > -1) {
-  //       //console.log("emotion:" + emotion + " matches genre:" + genreKey);
-  //       matches = true;
-  //     }
-  //     else {
-  //       //console.log("emotion:" + emotion + " does NOT match genre:" + genreKey);
-  //       matches = false;
-  //     }
-  //   }, this);
-  //
-  //   if (matches) {
-  //     //console.log("emotions:" + emotions + " matches genre:" + genreKey);
-  //     matching_genres.push(genreKey)
-  //   }
-  // }
-  //
-  // console.log("emotions:" + emotions + " matches genres:" + matching_genres);
-  return matching_genres;
-};
+  return matchingGenres;
+}
 
 module.exports = {
-  genresforEmotions: genresforEmotions,
+  genresforEmotions: genresforEmotions
 };
--- src/emotionGenreMapper.test.js
+++ src/emotionGenreMapper.test.js
@@ -1,35 +1,48 @@
 var emotionGenreMapper = require('./emotionGenreMapper');
 
 describe('mapEmotionsTo', function () {
-  it('maps a single emotion (scared) to one or more genres', function () {
-    var result = emotionGenreMapper.genresforEmotions(['scared']);
-    expect(result).to.include('Horror');
-    expect(result).to.include('War');
-  });
-  it('maps multiple emotion to one or more genres', function () {
-    var result = emotionGenreMapper.genresforEmotions(['scared', 'frightened']);
-    expect(result).to.include('Horror');
-    expect(result).not.to.include('War');
-  });
-  it('maps happy emotion to one or more genres', function () {
-    var result = emotionGenreMapper.genresforEmotions(['happy']);
-    expect(result).to.include('Comedy');
-    expect(result).to.include('Animation');
-    expect(result).not.to.include('Drama');
-    expect(result).not.to.include('War');
+  describe('single emotion mapping', function () {
+    it('maps "happy" to several genres', function () {
+      var result = emotionGenreMapper.genresforEmotions(['happy']);
+      expect(result).to.include('Comedy');
+      expect(result).to.include('Animation');
+      expect(result).not.to.include('Drama');
+      expect(result).not.to.include('War');
+    });
+
+    it('maps "scared" to several genres', function () {
+      var result = emotionGenreMapper.genresforEmotions(['scared']);
+      expect(result).not.to.include('Comedy');
+      expect(result).not.to.include('Animation');
+      expect(result).to.include('Horror');
+      expect(result).to.include('War');
+    });
   });
 
-  it('maps multiple emotion to one or more genres', function () {
+  describe('multiple emotion mapping', function () {
+    it('maps "scared" and "frightened" to several genres', function () {
+      var result = emotionGenreMapper.genresforEmotions(['scared', 'frightened']);
+      expect(result).to.include('Horror');
+      expect(result).not.to.include('War');
+    });
+
+    it('maps "excited" and "exhilarated" to several genres', function () {
       var result = emotionGenreMapper.genresforEmotions(['excited', 'exhilarated']);
       expect(result).to.include('Action');
       expect(result).to.include('Adventure');
       expect(result).not.to.include('Comedy');
-  });
-
-    it('maps multiple emotion to one or more genres', function () {
-        var result = emotionGenreMapper.genresforEmotions(['excited', 'exhilarated', 'inspired']);
-        expect(result).not.to.include('Action');
-        expect(result).to.include('Adventure');
-        expect(result).to.include('Sci-Fi');
     });
+
+    it('maps "excited", "exhilarated", "inspired" to several genres', function () {
+      var result = emotionGenreMapper.genresforEmotions(['excited', 'exhilarated', 'inspired']);
+      expect(result).not.to.include('Action');
+      expect(result).to.include('Adventure');
+      expect(result).to.include('Sci-Fi');
+    });
+
+    it('maps "excited", "exhilarated", "inspired" to no genres', function () {
+      var result = emotionGenreMapper.genresforEmotions(['happy', 'sad']);
+      expect(result).to.have.length(0);
+    });
+  });
 });
