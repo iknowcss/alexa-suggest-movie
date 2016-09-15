6ecbae9de55abaddd1c1745ec4fa69688c896691
Kurt Brown <kurtbrown@Kurts-MacBook.local>
2016-09-15 10:51:45 +1000

    first draft of emotion to genre matching

--- src/mapEmotionsToGenres.js
+++ src/mapEmotionsToGenres.js
@@ -1,3 +1,28 @@
+var genre_emotions = require('../data/genre_emotions.json')
+
 module.exports = function (emotions) {
-  return ['Horror'];
+
+  var matching_genres = [];
+
+  console.log("looking for genres for these emotions:"+emotions);
+
+  emotions.forEach(function(emotion) {
+
+    console.log("matching emotion:"+emotion);
+       for(var genreKey in genre_emotions) {
+
+         console.log("key:" + genreKey + ", value:" + genre_emotions[genreKey]);
+
+         if (genre_emotions[genreKey].indexOf(emotion) > -1) {
+           console.log("emotion:" + emotion + " matches genre:" + genreKey);
+           matching_genres.push(genreKey)
+         }
+       }
+
+
+    }, this);
+
+  console.log("emotions:" + emotions + " matches genres:" + matching_genres);
+  return matching_genres;
+  //return ["Horror"];
 };
