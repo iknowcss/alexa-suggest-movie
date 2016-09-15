8480c77402ccb621fc11703e7a432dad2e13da51
Cody Jenkins <iknowcss@gmail.com>
2016-09-15 12:35:31 +1000

    Change spacing from 4 to 2

--- src/index.js
+++ src/index.js
@@ -8,38 +8,38 @@ const SKILL_NAME = 'Suggest Movie';
 movieRepository.init();
 
 const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetMovie');
-    },
+  'LaunchRequest': function () {
+    this.emit('GetMovie');
+  },
 
-    'SuggestMovie': function () {
-        this.emit(':tell', 'Let me see what I can find');
-        this.emit('GetMovie');
-    },
+  'SuggestMovie': function () {
+    this.emit(':tell', 'Let me see what I can find');
+    this.emit('GetMovie');
+  },
 
-    'GetMovie': function () {
-        // Create speech output
-        var emotion = this.event.request.intent.slots.emotion.value;
+  'GetMovie': function () {
+    // Create speech output
+    var emotion = this.event.request.intent.slots.emotion.value;
 
-        movieRepository.getAllMovies()
-          .then(function (movies) {
-            var movie = movies[0];
-            var speechOutput = 'You will feel ' + emotion + 'after watching ' + movie.title;
+    movieRepository.getAllMovies()
+      .then(function (movies) {
+        var movie = movies[0];
+        var speechOutput = 'You will feel ' + emotion + 'after watching ' + movie.title;
 
-            this.emit(':tellWithCard', speechOutput, SKILL_NAME, movie.title);
-          });
-    },
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, movie.title);
+      });
+  },
 
-    'AMAZON.HelpIntent': function () {
-        var speechOutput = 'You can say suggest me a movie  , or, you can say exit... What can I help you with?';
-        var reprompt = 'What can I help you with?';
-        this.emit(':ask', speechOutput, reprompt);
-    }
+  'AMAZON.HelpIntent': function () {
+    var speechOutput = 'You can say suggest me a movie  , or, you can say exit... What can I help you with?';
+    var reprompt = 'What can I help you with?';
+    this.emit(':ask', speechOutput, reprompt);
+  }
 };
 
 exports.handler = function (event, context) {
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  var alexa = Alexa.handler(event, context);
+  alexa.APP_ID = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
