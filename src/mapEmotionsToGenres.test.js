b675e31f519bf72dcda625241b7373b8316f62cb
Kurt Brown <kurtbrown@Kurts-MacBook.local>
2016-09-15 11:32:24 +1000

    match genres based on all emotions

--- src/mapEmotionsToGenres.test.js
+++ src/mapEmotionsToGenres.test.js
@@ -1,18 +1,35 @@
 var mapEmotionsToGenres = require('./mapEmotionsToGenres');
 
 describe('mapEmotionsTo', function () {
-  it('maps a single emotion to one or more genres', function () {
+  it('maps a single emotion (scared) to one or more genres', function () {
     var result = mapEmotionsToGenres(['scared']);
     expect(result).to.include('Horror');
+    expect(result).to.include('War');
   });
-  it('maps a single emotion to one or more genres', function () {
+  it('maps multiple emotion to one or more genres', function () {
+    var result = mapEmotionsToGenres(['scared', 'frightened']);
+    expect(result).to.include('Horror');
+    expect(result).not.to.include('War');
+  });
+  it('maps happy emotion to one or more genres', function () {
     var result = mapEmotionsToGenres(['happy']);
     expect(result).to.include('Comedy');
     expect(result).to.include('Animation');
-  });
-  it('maps a single emotion to one or more genres', function () {
-    var result = mapEmotionsToGenres(['delighted']);
     expect(result).not.to.include('Drama');
     expect(result).not.to.include('War');
   });
+
+  it('maps multiple emotion to one or more genres', function () {
+      var result = mapEmotionsToGenres(['excited', 'exhilarated']);
+      expect(result).to.include('Action');
+      expect(result).to.include('Adventure');
+      expect(result).not.to.include('Comedy');
+  });
+
+    it('maps multiple emotion to one or more genres', function () {
+        var result = mapEmotionsToGenres(['excited', 'exhilarated', 'inspired']);
+        expect(result).not.to.include('Action');
+        expect(result).to.include('Adventure');
+        expect(result).to.include('Sci-Fi');
+    });
 });
