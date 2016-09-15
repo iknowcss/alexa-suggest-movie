a499e46b8f418916573369938259e1b67b91625b
Kurt Brown <kurtbrown@Kurts-MacBook.local>
2016-09-15 11:07:58 +1000

    add tests for emotion matching

--- src/mapEmotionsToGenres.test.js
+++ src/mapEmotionsToGenres.test.js
@@ -2,6 +2,17 @@ var mapEmotionsToGenres = require('./mapEmotionsToGenres');
 
 describe('mapEmotionsTo', function () {
   it('maps a single emotion to one or more genres', function () {
-    expect(mapEmotionsToGenres(['scared'])).to.include.members(['Horror']);
+    var result = mapEmotionsToGenres(['scared']);
+    expect(result).to.include('Horror');
+  });
+  it('maps a single emotion to one or more genres', function () {
+    var result = mapEmotionsToGenres(['happy']);
+    expect(result).to.include('Comedy');
+    expect(result).to.include('Animation');
+  });
+  it('maps a single emotion to one or more genres', function () {
+    var result = mapEmotionsToGenres(['delighted']);
+    expect(result).not.to.include('Drama');
+    expect(result).not.to.include('War');
   });
 });
