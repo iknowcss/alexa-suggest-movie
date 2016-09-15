026b3262a18ed71932ba61ec42d66a124e12f2da
Arvind Chaudhary <achaudhary@dius.com.au>
2016-09-15 11:38:10 +1000

    Updated the lambda function

--- src/index.js
+++ src/index.js
@@ -5,31 +5,32 @@ var APP_ID = '[your app id]';
 var SKILL_NAME = 'Suggest Movie';
 
 var handlers = {
-  'LaunchRequest': function () {
-    this.emit('GetMovie');
-  },
-  
-  'SuggestMovie': function () {
-    this.emit('GetMovie');
-  },
-  
-  'GetMovie': function () {
-    // Create speech output
-    var speechOutput = 'You will love to watch DealPool';
+    'LaunchRequest': function () {
+        this.emit('GetMovie');
+    },
 
-    this.emit(':tellWithCard', speechOutput, SKILL_NAME, 'DealPool');
-  },
-  
-  'AMAZON.HelpIntent': function () {
-    var speechOutput = 'You can say suggest me a movie  , or, you can say exit... What can I help you with?';
-    var reprompt = 'What can I help you with?';
-    this.emit(':ask', speechOutput, reprompt);
-  }
+    'SuggestMovie': function () {
+        this.emit('GetMovie');
+    },
+
+    'GetMovie': function () {
+        // Create speech output
+        var emotion = this.event.request.intent.slots.emotion.value;
+        var speechOutput = 'You will feel ' + emotion + 'after watching DeadPool';
+
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, 'DeadPool');
+    },
+
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = 'You can say suggest me a movie  , or, you can say exit... What can I help you with?';
+        var reprompt = 'What can I help you with?';
+        this.emit(':ask', speechOutput, reprompt);
+    }
 };
 
 exports.handler = function (event, context) {
-  var alexa = Alexa.handler(event, context);
-  alexa.APP_ID = APP_ID;
-  alexa.registerHandlers(handlers);
-  alexa.execute();
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
